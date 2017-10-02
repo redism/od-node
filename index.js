4f2ebd73d736cc363e33019971625361ef308bf1
Jeff Oh <jeff.oh@taggle.co>
2017-10-02 20:16:27 +0900

    bcryptjs 를 이용한 패스워드 생성 추가, knex export.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const mysql = require('mysql')
+const bcrypt = require('bcryptjs')
+const Knex = require('knex')
+const knex = Knex({ client: 'mysql' }) // use only for query-builder
 
 function getMySQLConnection () {
   return mysql.createConnection({
@@ -11,6 +14,47 @@ function getMySQLConnection () {
   })
 }
 
+function genSaltedPassword (pw, iteration = 10, cb) {
+  bcrypt.genSalt(iteration, (err, salt) => {
+    if (err) {
+      cb(err)
+    } else {
+      bcrypt.hash(pw, salt, (err, salted) => {
+        if (err) {
+          cb(err)
+        } else {
+          const sqlInsertAdmin = knex('setting').insert({
+            email: 'jeff.oh@overdosed.co.kr',
+            pw: salted,
+            forcedUpdateVersion: null,
+          }).toString()
+
+          const conn = getMySQLConnection()
+          const sqlResetSetting = knex('setting').delete().toString()
+          conn.query(sqlResetSetting, {}, (err, res) => {
+            if (err) {
+              cb(err)
+            } else {
+              conn.query(sqlInsertAdmin, {}, (err, res) => {
+                conn.end()
+                if (err) {
+                  cb(err)
+                } else {
+                  console.log(res)
+                  cb(null, `Ran SQL successfully => ${res}`)
+                }
+              })
+            }
+          })
+        }
+      })
+    }
+  })
+}
+
 module.exports = exports = {
   getMySQLConnection: getMySQLConnection,
+  genSaltedPassword,
+  knex,
+  Knex,
 }
