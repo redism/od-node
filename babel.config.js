0a621bd546b0d9290cd7e7e75473c2b58dd5a28f
Jeff Oh <redism@gmail.com>
2019-02-19 09:56:20 +0900

    Upgrade versions of dependencies. Use babel 7 with new babel.config.js.

---
+++ babel.config.js
@@ -0,0 +1,33 @@
+module.exports = function (api) {
+  api.cache(true)
+
+  const ignore = [
+    'node_modules', 'babel.config.js', /package*.json/, 'npm-debug.log',
+  ]
+
+  const presets = [
+    [
+      '@babel/env',
+      {
+        targets: { node: true },
+      },
+    ],
+  ]
+
+  const plugins = [
+    // Stage 2
+    [ '@babel/plugin-proposal-decorators', { legacy: true } ],
+    '@babel/plugin-proposal-function-sent',
+    '@babel/plugin-proposal-export-namespace-from',
+    '@babel/plugin-proposal-numeric-separator',
+    '@babel/plugin-proposal-throw-expressions',
+
+    // Stage 3
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-syntax-import-meta',
+    [ '@babel/plugin-proposal-class-properties', { loose: false } ],
+    '@babel/plugin-proposal-json-strings',
+  ]
+
+  return { presets, plugins, ignore }
+}
